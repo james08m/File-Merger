cf9b62b86e6079786978d308beb62e850faa1e97
james08m <james08major@gmail.com>
2019-01-25 21:27:03 -0500

    Correction of small bugs.

--- merger.py
+++ merger.py
@@ -8,42 +8,42 @@ if len(sys.argv) >= 3 and len(sys.argv) <= 4:  # Must have 2 or 3 arguments pass
     file_ids = open(sys.argv[1], "r")
     file_ips = open(sys.argv[2], "r")
 
+    result_path = "result.txt"
+
     if len(sys.argv) == 4:
-        file_result = open(sys.argv[3], "w+")
-    else:
-        file_result = open("result.txt", "w+")
+        result_path= sys.argv[3]
 
-    ids    = []
-    ips    = []
-    temp   = []
+    file_result = open(result_path, "w+")
 
+    # Create a list with a the lines in the text file
     ids = file_ids.readlines()
     ips = file_ips.readlines()
+    temp = [] # Temporary list
 
-    print "IDs File and IPs file merger"
     print "Loaded ids : %s" % len(ids)
     print "Loaded ips : %s" % len(ips)
 
     if len(ids) == len(ips):
+        print "Start Parsing ids files"
         for line in ids:
             line = line[:2] + ';' + line[2:] # Separate Province code from site code
             line = line[:8] + ';' + line[8:] # Separate site code from till code
             line = line[:11]                 # Croped unnecessary information from line 
             temp.append(line)
-        print "Created temp list : %s" % len(temp)
 
         i=0
+        print "Finale parsing and joining files lines"
         while i < len(ips):
             line = temp[i] + ';' + ips[i]    # Separate ids from ips
             file_result.write(line)          # Write result to file
             i = i+1
 
-        
+        print "Task completed, results in file : %s" % result_path
     else:
         print "Files lines amount does not match!"
 
-    file_a.close()
-    file_b.close()
+    file_ids.close()
+    file_ips.close()
     file_result.close()
 else:
     print "Insufficient or too much arguments passed to program."
