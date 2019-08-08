2c6c738a5a10d1f6dbbbda6e058abb11000cd716
james08m <james08major@gmail.com>
2019-08-08 08:53:32 -0400

    Update code comments amd readme.

--- merger.py
+++ merger.py
@@ -1,18 +1,22 @@
 import sys
 import time
 
+## Program required minimum 2 arguments to run.
+## First argument is the path to the file containing the ids.
+## Second argument is the path to the file containing the IPs.
+## Third argument is the result file path (not mandatory).
+if len(sys.argv) >= 3 and len(sys.argv) <= 4:
 
-
-if len(sys.argv) >= 3 and len(sys.argv) <= 4:  # Must have 2 or 3 arguments passed to program
-    
+    # Open and read files
     file_ids = open(sys.argv[1], "r")
     file_ips = open(sys.argv[2], "r")
 
+    # Default result file
     result_path = "sites.info"
 
+    # Checked if there a 3rd argument
     if len(sys.argv) == 4:
         result_path= sys.argv[3]
-
     file_result = open(result_path, "w+")
 
     # Create a list with a the lines in the text file
@@ -23,6 +27,7 @@ if len(sys.argv) >= 3 and len(sys.argv) <= 4:  # Must have 2 or 3 arguments pass
     print "Loaded ids : %s" % len(ids)
     print "Loaded ips : %s" % len(ips)
 
+    # Make sure thre is as much IDs as IPs
     if len(ids) == len(ips):
         print "Start Parsing files"
         for line in ids:
@@ -42,6 +47,7 @@ if len(sys.argv) >= 3 and len(sys.argv) <= 4:  # Must have 2 or 3 arguments pass
     else:
         print "Files lines amount does not match!"
 
+    # Properly close the files used
     file_ids.close()
     file_ips.close()
     file_result.close()
